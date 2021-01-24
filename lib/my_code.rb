f24a35feb925fc90f8a0edcd85ed85bcdc6c14a0
Bleak Chandler <bleakchandler@gmail.com>
2021-01-24 22:41:22 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -12,7 +12,7 @@ def reduce (array, starting_number = 0)
   count = 0
   starting_point = starting_number
   while i < array.length do
-     starting_point = yield(value,array[count])
+     starting_point = yield(starting_point,array[count])
     count += 1
   end
  }
