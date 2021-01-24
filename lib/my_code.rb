444223e692933487820de7fc83ffc2524ece8aa3
Bleak Chandler <bleakchandler@gmail.com>
2021-01-24 22:27:53 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,22 @@
-# Your Code Here
+def map(s)
+  new = []
+  i = 0
+  while i < s.length do
+    new.push(yield(s[i])) 
+    i += 1
+  end
+  return new
+end
+
+def reduce(s, n = 0)
+  new = n
+ 
+  starting = n
+ s.each{ |number|
+    new = number + new 
+  end
+ }
+  return new
+end
+
+reduce([1, 2, 3], 100)
