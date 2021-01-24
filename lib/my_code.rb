8ac23051926defbe6d776592ac92d27bfa23da1d
Bleak Chandler <bleakchandler@gmail.com>
2021-01-24 23:10:58 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,11 +10,11 @@ end
 
 def reduce (array, *starting_number)
 
-if starting_number
+if starting_number[0]
   count = 0
-  total = starting_number
+  total = starting_number[0]
 else
-  total = array
+  total = array[0]
   count = 1
 end
   
