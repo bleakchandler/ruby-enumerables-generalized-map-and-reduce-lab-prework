a32067fdc425c8539083191bbe0b6dfb1076d344
Bleak Chandler <bleakchandler@gmail.com>
2021-01-24 23:12:49 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -12,7 +12,7 @@ def reduce (array, *starting_number)
 
 if starting_number[0]
   count = 0
-  total = starting_numbber[0]
+  total = starting_number[0]
 else
   total = array[0]
   count = 1
@@ -29,7 +29,7 @@ end
 
 source_array = [1,2,3]
 starting_point = 100
-reduce(source_array, starting_point){|memo, n| memo + n}
+reduce(source_array, st){|memo, n| memo + n}
 
 
 
