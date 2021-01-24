b3a16d7c1635e1d8b8575ebdb091268e02844010
Bleak Chandler <bleakchandler@gmail.com>
2021-01-24 22:49:24 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -18,7 +18,7 @@ def reduce (array, starting_number = 0)
   return starting_point
 end
 
-source_array = [1,2,3]
-starting_point = 100
+#source_array = [1,2,3]
+#starting_point = 100
     
-reduce(source_array, starting_point){|memo, n| memo + n}
+#reduce(source_array, starting_point){|memo, n| memo + n}
